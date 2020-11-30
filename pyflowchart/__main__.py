5846f76402291b47e917d717447508e26d3cbd8e
cdfmlr <cdfmlr@163.com>
2020-11-30 18:26:03 +0800

    CLI support flowchart special field.
    for #2
    
    ## __main__.py
    
    - import argparse to parse CLI args
    - add optional arguments f, i and h (argparse help)
        -f --field: field to draw flowchart. (e.g. Class.method)
        -i --inner: parse the body of field
            (without -i: parse ast_obj(field))
    
    ## CLI usage:
        __main__.py [-h] [-f FIELD] [-i] code_file

--- pyflowchart/__main__.py
+++ pyflowchart/__main__.py
@@ -7,16 +7,27 @@ license that can be found in the LICENSE file.
 """
 
 import sys
+import argparse
 
 from pyflowchart.flowchart import Flowchart
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("pyflowchart: Python code to flowchart\nUsage: pyflow py_file")
 
-    file_name = sys.argv[1]
-    with open(file_name) as f:
-        code = f.read()
-
-    flowchart = Flowchart.from_code(code)
+def main(code_file, field, inner):
+    code = code_file.read()
+    flowchart = Flowchart.from_code(code, field=field, inner=inner)
     print(flowchart.flowchart())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Python code to flowchart.')
+
+    parser.add_argument('code_file',  type=argparse.FileType('r'))
+    parser.add_argument('-f', '--field', default="", type=str, help="field to draw flowchart. (e.g. Class.method)")
+    parser.add_argument('-i', '--inner', action="store_true", help="parse the body of field")
+
+    args = parser.parse_args()
+
+    if not args.field:  # field="", parse the whole file (ast Module), should use the body
+        args.inner = True
+
+    main(args.code_file, args.field, args.inner)
