3f40207cf56f709ce6b96fd8bd894c7812e7d54b
cdfmlr <cdfmlr@163.com>
2021-03-07 14:23:39 +0800

    update README, boost version
    - arrange Advanced Usages section
    - describe new `--no-simplify`
    - v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rb") as fh:
 
 setuptools.setup(
     name='pyflowchart',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/cdfmlr/pyflowchart',
     license='MIT',
     author='CDFMLR',
