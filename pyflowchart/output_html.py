12d13ad45ac14a686f4fdbd1fb55bee1213cda77
Capcom Coleman <capcom.coleman@cyburity.com>
2023-08-03 19:29:17 -0500

    download PNG or SVG from html page! Added javascript to generate and provide download links for the SVG of the current run/rendered flowchart. So you can tweak to what you like and then download them. There were issues with XML because of the string "Created with Raphaël" so I replace the ë to e during conversion. Because of this in the javascript I now specify writing the html file as UTF-8 encoding as well.

--- pyflowchart/output_html.py
+++ pyflowchart/output_html.py
@@ -36,7 +36,7 @@ def output_html(output_name: str, field_name: str, flowchart: str) -> None:
                 var btn = document.getElementById("run"),
                     cd = document.getElementById("code"),
                     chart;
-
+					
                 (btn.onclick = function () {
                     var code = cd.value;
 
@@ -107,15 +107,112 @@ def output_html(output_name: str, field_name: str, flowchart: str) -> None:
                         'rejected' : { 'fill' : '#C45879', 'font-size' : 12, 'yes-text' : 'n/a', 'no-text' : 'REJECTED' }
                       }
                     });
+					//create base64 encoding of SVG to generate download link for title(without html or htm).SVG
+					var currentCanvasDIV = document.getElementById('canvas')
+					var currentDrawSVG = currentCanvasDIV.innerHTML.replaceAll('ë','e');
 
-                })();
+					const OUTsvgBASE64 = btoa(currentDrawSVG)
+					doctitle = document.title.replace('.html','');
+					doctitle = doctitle.replace('.htm','');
 
-            };
-            function myFunction(event, node) {
-              console.log("You just clicked this node:", node);
+
+					var currentCanvasDIV = document.getElementById('canvas')
+					var currentDrawSVG = currentCanvasDIV.innerHTML.replaceAll('ë','e');
+					svgSource = currentDrawSVG
+					svgXML = currentDrawSVG;
+					// Use SVG Height and Width from the SVG XML to set canvas size
+					svgXMLsubstringHeight = svgXML.substring(svgXML.indexOf('height='), svgXML.indexOf('version='));
+					svgXMLsubstringWidth = svgXML.substring(svgXML.indexOf('width='), svgXML.indexOf('xmlns='));
+					HeightValue = svgXMLsubstringHeight.substring(svgXMLsubstringHeight.indexOf('"')+1,svgXMLsubstringHeight.lastIndexOf('"'));
+					WidthValue = svgXMLsubstringWidth.substring(svgXMLsubstringWidth.indexOf('"')+1,svgXMLsubstringWidth.lastIndexOf('"'));
+					HeightValueInt = Math.round(HeightValue)
+					WidthValueInt = Math.round(WidthValue)
+					// setup input for base64SvgToBase64Png
+					let svgSrc = "data:image/svg+xml;base64,"+OUTsvgBASE64;
+					var pngBase
+					imageUtil.base64SvgToBase64Png(svgSrc, WidthValueInt, HeightValueInt).then(pngSrc => {
+					pngBase = pngSrc
+					// output download link for base64 PNG converted on download from base64
+					var pngOutHtml = `<a href="${pngBase}" download="${doctitle}.png">PNG - Click here to download current rendered flowchart as ${doctitle}.png</a>`
+					document.getElementById("pngbase64").innerHTML=pngOutHtml;
+					});	
+					// output download link for base64 SVG converted on download from base64
+					var svgOutHtml = `<a href="data:image/svg+xml;base64,${OUTsvgBASE64}" download=${doctitle}.svg>SVG - Click here to download current rendered flowchart as ${doctitle}.svg</a> `
+						document.getElementById("svgbase64").innerHTML=svgOutHtml;
+					})();
+
+							};
+				 
+
+// derived from https://stackoverflow.com/a/64800570
+// we need to use web browser canvas to generate a image. In this case png
+let imageUtil = {};
+/**
+ * converts a base64 encoded data url SVG image to a PNG image
+ * @param originalBase64 data url of svg image
+ * @param width target width in pixel of PNG image
+ * @param secondTry used internally to prevent endless recursion
+ * @return {Promise<unknown>} resolves to png data url of the image
+ */
+imageUtil.base64SvgToBase64Png = function (originalBase64, width, height, secondTry) {
+    return new Promise(resolve => {
+		let img = document.createElement('img');
+        img.onload = function () {
+            if (!secondTry && (img.naturalWidth === 0 || img.naturalHeight === 0)) {
+                let svgDoc = base64ToSvgDocument(originalBase64);
+                let fixedDoc = fixSvgDocumentFF(svgDoc);
+                return imageUtil.base64SvgToBase64Png(svgDocumentToBase64(fixedDoc), width, height, true).then(result => {
+                    resolve(result);
+                });
             }
-            
+            //document.body.appendChild(img);
+            let canvas2 = document.createElement("canvas");
+            //document.body.removeChild(img);
+            canvas2.width = width;
+            canvas2.height = height;
+            let ctx = canvas2.getContext("2d");
+            ctx.drawImage(img, 0, 0, canvas2.width, canvas2.height);
+            try {
+                let data = canvas2.toDataURL('image/png');
+                resolve(data);
+            } catch (e) {
+                resolve(null);
+            }
+        };
+        img.src = originalBase64;
+    });
+}
+
+//needed because Firefox doesn't correctly handle SVG with size = 0, see https://bugzilla.mozilla.org/show_bug.cgi?id=700533
+function fixSvgDocumentFF(svgDocument) {
+    try {
+        let widthInt = parseInt(svgDocument.documentElement.width.baseVal.value) || 500;
+        let heightInt = parseInt(svgDocument.documentElement.height.baseVal.value) || 500;
+        svgDocument.documentElement.width.baseVal.newValueSpecifiedUnits(SVGLength.SVG_LENGTHTYPE_PX, widthInt);
+        svgDocument.documentElement.height.baseVal.newValueSpecifiedUnits(SVGLength.SVG_LENGTHTYPE_PX, heightInt);
+        return svgDocument;
+    } catch (e) {
+        return svgDocument;
+    }
+}
+
+function svgDocumentToBase64(svgDocument) {
+    try {
+        let base64EncodedSVG = btoa(new XMLSerializer().serializeToString(svgDocument));
+        return 'data:image/svg+xml;base64,' + base64EncodedSVG;
+    } catch (e) {
+        return null;
+    }
+}
+
+function base64ToSvgDocument(base64) {
+    let svg = atob(base64.substring(base64.indexOf('base64,') + 7));
+    svg = svg.substring(svg.indexOf('<svg'));
+    let parser = new DOMParser();
+    return parser.parseFromString(svg, "image/svg+xml");
+} 
         </script>
+
 		<script>
 			function HelpText() {
 			  var x = document.getElementById("HelpTextBlock");
@@ -132,19 +229,19 @@ def output_html(output_name: str, field_name: str, flowchart: str) -> None:
 
     html_part_after_code_block_remaining_html="""</textarea></div>
         <div><button id="run" type="button">Run</button> <button onclick="HelpText()">Format Help</button></div>
-        
-		<div id="HelpTextBlock" style="display:none">Conditions can also be redirected like cond(yes, bottom) or cond(yes, right)
+		<div id="HelpTextBlock" style="display:none"><br/>Conditions can also be redirected like cond(yes, bottom) or cond(yes, right)
 ... and the other symbols too... like sub1(right)<br/>
-You can also tweak the diagram.drawSVG('diagram', {}); script in this file for more changes<br/>
+You can also tweak the <b>diagram.drawSVG('diagram', {});</b> script in this file for more changes<br/>
 This is based on <a href="https://github.com/adrai/flowchart.js">flowchart.js on github</a> and <a href="http://flowchart.js.org">http://flowchart.js.org</a> more documentation can be found over there.
-</div>
+</div><br/><div id="svgbase64"></div>
+		<div id="pngbase64"></div>
 
         <div id="canvas"></div>
     </body>
 </html>"""
     # No checking or prompting if output file already exists.
     # This will overwrite if given an existing output file
-    with open(output_name, 'w') as f:
+    with open(output_name, 'w', encoding="utf-8") as f:
         # html_part_before_title is header before <title>, 
         f.write(html_part_before_title)
         # If field specified use that as title. If not, use output filename as title.
