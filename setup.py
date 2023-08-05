e9e72718f504d1884198cae41d58e1aed515c4c1
CDFMLR <cdfmlr@163.com>
2023-08-05 11:13:56 +0800

    docs & export the output_html()

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rb") as fh:
 
 setuptools.setup(
     name='pyflowchart',
-    version='0.3.0-alpha.3',
+    version='0.3.0-beta.1',
     url='https://github.com/cdfmlr/pyflowchart',
     license='MIT',
     author='CDFMLR',
